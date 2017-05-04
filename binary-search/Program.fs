da6ae8913662816cc6f97dc04a04e2031ad30f7a
Shaun Luttin <shaunluttin@builddirect.com>
2017-05-03 18:02:26 -0700

    Begin binarySearch

---
+++ binary-search/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let rec binarySearch (array: int[]) = 
+    printfn "binarySearch"
+    printfn "%A" array.Length
+    let slice = array.[0..(array.Length / 2 - 1)]
+    Console.ReadKey();
+    match slice with 
+    | [||] -> printfn "Empty"
+    | _ -> binarySearch slice
+
+[<EntryPoint>]
+let main argv =
+    binarySearch [|0..10|]
+    0 
