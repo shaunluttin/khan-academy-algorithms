03bb1e331ee371b1f6a1fdc1b722c9f7ce6819a2
Shaun Luttin <admin@shaunluttin.com>
2017-05-03 21:01:47 -0700

    Handle failed search

--- 01-linear-search/Program.fs
+++ 01-linear-search/Program.fs
@@ -1,21 +1,22 @@
 ﻿open System
 
 let rec linearSearch (array: string[]) (target: string) (index: int) = 
-    let next = array.[index]
-    match next with 
-    | x when x = target -> index
-    | y -> linearSearch array target (index + 1)
+    match array.Length with 
+    | l when index >= l -> -1
+    | _ -> 
+        match array.[index] with 
+        | x when x = target -> index
+        | y -> linearSearch array target (index + 1)
 
 [<EntryPoint>]
 let main argv =
-    linearSearch [|
+    let names = [|
         "erdnase"
         "vernon"
-        "ortiz"
-        "england" // 3
-        "grant"
-        "bernahart"
-        "barker"
-        "covernton"
-    |] "england" 0 |> printfn "The target is at index: %A" 
+        "le paul"
+        "scarne"
+        "ireland"
+    |]
+    linearSearch names "le paul" 0 |> printfn "The target is at index: %A" 
+    linearSearch names "luttin" 0 |> printfn "The target is at index: %A" 
     0 
