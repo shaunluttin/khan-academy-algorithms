398262cdc9fb0f59bcb244ccb871d55ad12f337e
Shaun Luttin <admin@shaunluttin.com>
2017-05-03 20:33:54 -0700

    Write linear search

--- 01-linear-search/Program.fs
+++ 01-linear-search/Program.fs
@@ -1,8 +1,21 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let rec linearSearch (array: string[]) (target: string) (index: int) = 
+    let next = array.[index]
+    match next with 
+    | x when x = target -> index
+    | y -> linearSearch array target (index + 1)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    linearSearch [|
+        "erdnase"
+        "vernon"
+        "ortiz"
+        "england" // 3
+        "grant"
+        "bernahart"
+        "barker"
+        "covernton"
+    |] "england" 0 |> printfn "The target is at index: %A" 
+    0 
