8569ed9d401f891c7b500cf750fb9aac1cf27cbc
Shaun Luttin <admin@shaunluttin.com>
2017-05-09 19:31:35 -0700

    Wrap the recursive call

--- 01-linear-search/Program.fs
+++ 01-linear-search/Program.fs
@@ -1,12 +1,14 @@
 ﻿open System
 
-let rec linearSearch (array: string[]) (target: string) (index: int) = 
-    match array.Length with 
-    | l when index >= l -> -1
-    | _ -> 
-        match array.[index] with 
-        | x when x = target -> index
-        | y -> linearSearch array target (index + 1)
+let linearSearch (array: string[]) (target: string) : Option<int> = 
+    let rec search index = 
+        match array.Length with 
+        | l when index >= l -> None
+        | _ -> 
+            match array.[index] with 
+            | x when x = target -> Some index
+            | y -> search (index + 1)
+    search 0
 
 [<EntryPoint>]
 let main argv =
@@ -17,6 +19,6 @@ let main argv =
         "scarne"
         "ireland"
     |]
-    linearSearch names "le paul" 0 |> printfn "The target is at index: %A" 
-    linearSearch names "luttin" 0 |> printfn "The target is at index: %A" 
+    linearSearch names "le paul" |> printfn "The target is at index: %A" 
+    linearSearch names "luttin" |> printfn "The target is at index: %A" 
     0 
