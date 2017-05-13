8fba5d3681ceea397c0a06fad88d77d6fd3401b3
Shaun Luttin <shaunluttin@builddirect.com>
2017-05-12 20:36:00 -0700

    Complete steps 1..2 of binary search

--- 02-binary-search/Program.fs
+++ 02-binary-search/Program.fs
@@ -1,15 +1,24 @@
 ﻿open System
 
-let rec binarySearch (array: int[]) = 
+let binarySearch (array: int[]) = 
     printfn "binarySearch"
-    printfn "%A" array.Length
-    let slice = array.[0..(array.Length / 2 - 1)]
-    Console.ReadKey();
-    match slice with 
-    | [||] -> printfn "Empty"
-    | _ -> binarySearch slice
+    let rec search min max =
+        printfn "search %i %i" min max
+        match (min, max) with 
+        | (min, max) when max < min -> -1
+        | _ -> search (min + 1) (max)
+    search (0) (array.Length - 1)
 
 [<EntryPoint>]
 let main argv =
     binarySearch [|0..10|]
     0 
+
+// 1. Let min = 0 and max = n-1.
+// 2. If max < min, then stop: target is not present in array. Return -1.
+
+// 3. Compute guess as the average of max and min, rounded down (so that it is an integer).
+// 4. If array[guess] equals target, then stop. You found it! Return guess.
+// 5. If the guess was too low, that is, array[guess] < target, then set min = guess + 1.
+// 6. Otherwise, the guess was too high. Set max = guess - 1.
+// 7. Go back to step 2.
