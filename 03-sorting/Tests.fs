3904cf8cde9266a1757871d00bac1d8aba60a873
Shaun Luttin <admin@shaunluttin.com>
2017-05-17 21:39:12 -0700

    Implement swap

--- 03-sorting/Tests.fs
+++ 03-sorting/Tests.fs
@@ -3,10 +3,20 @@ module Tests
 open System
 open Xunit
 
-let swap (array: int[]): int[] = 
-    [|1|]        
+let swap (array: int[]) (first: int) (second: int): int[] = 
+    let originalFirst = array.[first]
+    array.[first] <- array.[second];
+    array.[second] <- originalFirst;
+    array
 
 [<Fact>]
-let ``Swap test returns an array`` () =
-    let result = swap [|1..10|]
+let ``Swap returns an array`` () =
+    let result = swap [|0..2|] 0 1
     Assert.IsType<int[]>(result);
+
+[<Fact>]
+let ``Swap switches only the values at index zero and index one`` () =
+    let result = swap [|0..2|] 0 1
+    Assert.StrictEqual(result.[0], 1);
+    Assert.StrictEqual(result.[1], 0);
+    Assert.StrictEqual(result.[2], 2);
