ad4727025b526497700b53276e290106f4e8db27
Shaun Luttin <admin@shaunluttin.com>
2017-05-17 21:40:59 -0700

    Add a todo

--- 03-sorting/Tests.fs
+++ 03-sorting/Tests.fs
@@ -3,6 +3,7 @@ module Tests
 open System
 open Xunit
 
+// todo: Ask about this on codereview.stackexchange.com
 let swap (array: 'a[]) (first: int) (second: int): 'a[] = 
     let originalFirst = array.[first]
     array.[first] <- array.[second];
