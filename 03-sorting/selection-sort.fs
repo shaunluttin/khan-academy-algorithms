8f8a92c3aa7fa4658192d9eae51af65807bac2d5
Shaun Luttin <admin@shaunluttin.com>
2017-05-23 20:50:43 -0700

    Hack out an implementation of selection sort

--- 03-sorting/selection-sort.fs
+++ 03-sorting/selection-sort.fs
@@ -3,11 +3,24 @@ module SelectionSort
 open System
 open Xunit
 
+open FindMinimum
+open Swap
+
+// hack: This is ugly, and I can forgive myself, because I am learning.
 let selectionSort (someList: int list): int list = 
-    someList
+    let rec sort (newList: int list) (index: int) = 
+        if (index < someList.Length) 
+        then 
+            let minIndexOption = FindMinimum.findMinimumValueIndex newList.[index..]
+            let minIndex = minIndexOption.Value + index
+            let afterSwap = Swap.swap newList index minIndex
+            sort afterSwap (index + 1)
+        else newList
+    sort someList 0
 
 [<Fact>]
 let ``Selection sort puts numbers in order`` () =
     let list = [22; 11; 99; 88; 9; 7; 42];
     let result = selectionSort list
-    Assert.StrictEqual(result, [7; 9; 11; 22; 42; 88; 99])
+    printfn "%A" result
+    Assert.StrictEqual([7; 9; 11; 22; 42; 88; 99], result)
