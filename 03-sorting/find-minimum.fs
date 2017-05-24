bceff0dcd354d357a5b61a55fb4079f2a20f3185
Shaun Luttin <admin@shaunluttin.com>
2017-05-23 19:27:02 -0700

    Use list not array

--- 03-sorting/find-minimum.fs
+++ 03-sorting/find-minimum.fs
@@ -3,19 +3,19 @@ module FindMinimumTests
 open System
 open Xunit
 
-let findMinimumValueIndex (array: int[]): int =
-    let rec find (index: int) (minimumValueIndex: int) = 
+let findMinimumValueIndex (array: int list): int Option =
+    let rec find (index: int) (minimumValueIndex: int): int Option = 
         let findNext = find (index + 1)
         match index with 
-        | i when i >= array.Length -> minimumValueIndex // base case
+        | i when i >= array.Length -> Some minimumValueIndex // base case
         | i when array.[i] < array.[minimumValueIndex] -> findNext i
         | _ -> findNext minimumValueIndex
     find 0 0
 
 [<Fact>]
 let ``findMinimum returns the index of the minimum value in an array`` () = 
-    let array = [|4; 5; 6; 7; 3; 2; 7; 4; 6; 8; 3|]
+    let array = [4; 5; 6; 7; 3; 2; 7; 4; 6; 8; 3]
     let result = findMinimumValueIndex array
-    Assert.StrictEqual(result, 5);
+    Assert.StrictEqual(result, Some 5);
 
 
--- 03-sorting/swap.fs
+++ 03-sorting/swap.fs
@@ -3,22 +3,21 @@ module SwapTests
 open System
 open Xunit
 
-// todo: Ask about this on codereview.stackexchange.com. Done.
-// todo: Perform this using an immutable array (i.e. a list)
-let swap (array: 'a[]) (first: int) (second: int): 'a[] = 
-    let originalFirst = array.[first]
-    array.[first] <- array.[second];
-    array.[second] <- originalFirst;
-    array
+let swap (someList: 'a list) (first: int) (second: int): 'a list = 
+    List.mapi(fun i x -> 
+        if (i = first) then someList.[second]
+        elif (i = second) then someList.[first]
+        else x
+    ) someList
 
 [<Fact>]
 let ``Swap returns an array`` () =
-    let result = swap [|0..2|] 0 1
-    Assert.IsType<int[]>(result);
+    let result = swap [0..2] 0 1
+    Assert.IsType<int list>(result);
 
 [<Fact>]
 let ``Swap switches only the values at index zero and index one`` () =
-    let result = swap [|0..2|] 0 1
+    let result = swap [0..2] 0 1
     Assert.StrictEqual(result.[0], 1);
     Assert.StrictEqual(result.[1], 0);
     Assert.StrictEqual(result.[2], 2);
