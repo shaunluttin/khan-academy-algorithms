63d0fff612b36c1227e3e6f9cb122845eff88230
Shaun Luttin <admin@shaunluttin.com>
2017-05-23 19:48:52 -0700

    Add unit tests

---
+++ 01-linear-search/LinearSearch.fs
@@ -0,0 +1,48 @@
+﻿module LinearSearch
+
+open System
+open Xunit
+
+let linearSearch (someList: 'a list) (target: 'a) : Option<int> = 
+    let rec search index = 
+        match someList.Length with 
+        | l when index >= l -> None
+        | _ -> 
+            match someList.[index] with 
+            | x when x = target -> Some index
+            | y -> search (index + 1)
+    search 0
+
+let names = [
+    "erdnase"
+    "vernon"
+    "le paul"
+    "scarne"
+    "ireland"
+]
+
+let numbers = [
+    1
+    3
+    9
+]
+
+[<Fact>]
+let ``Finds string in list of strings`` () =
+    let result = linearSearch names "le paul" 
+    Assert.StrictEqual(result, Some 2)
+
+[<Fact>]
+let ``Does not find string in list of strings`` () =
+    let result = linearSearch names "luttin" 
+    Assert.StrictEqual(result, None)
+
+[<Fact>]
+let ``Finds int in list of ints`` () =
+    let result = linearSearch numbers 3 
+    Assert.StrictEqual(result, Some 1)
+
+[<Fact>]
+let ``Does not find int in list of ints`` () =
+    let result = linearSearch numbers 12 
+    Assert.StrictEqual(result, None)
---
+++ 02-binary-search/BinarySearch.fs
@@ -0,0 +1,39 @@
+﻿module BinarySearch
+
+open System
+open Xunit
+
+let binarySearch (array: 'a list) (target: 'a): int Option =
+    let rec search min max: int Option =
+        match (min, max) with 
+        | (min, max) when max < min -> None 
+        | _ -> 
+            let guess = ((min + max) / 2);
+            match array.[guess] with
+            | x when x > target -> search (min) (guess - 1)
+            | x when x < target -> search (guess + 1) (max) 
+            | _ -> Some guess
+    search (0) (array.Length - 1) 
+
+let sortedNames = [
+    "erdnase"
+    "ireland"
+    "le paul"
+    "scarne"
+    "vernon"
+]
+
+[<Fact>]
+let ``Binary search finds number`` () =
+    let result = binarySearch [0..10] 3 
+    Assert.StrictEqual(result, Some 3)
+
+[<Fact>]
+let ``Binary search finds string`` () =
+    let result = binarySearch sortedNames "le paul"
+    Assert.StrictEqual(result, Some 2)
+
+[<Fact>]
+let ``Binary search does not find string`` () =
+    let result = binarySearch sortedNames "luttin" 
+    Assert.StrictEqual(result, None)
